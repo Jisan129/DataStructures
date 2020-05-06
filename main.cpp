129f3c3bb6fe25569cc46c0564243ff606a521ec
Jisan129 <jishananam@iut-dhaka.edu>
2020-05-07 05:10:58 +0600

    Implementing doubly Linked List with the use of create function,insert,delete,insertafter,insertbefore ,length,sort

--- main.cpp
+++ main.cpp
@@ -47,20 +47,35 @@ int length(struct Node *p){
     }
     return sum;
 }
+void addFirst(struct Node *p,int key){
+    struct Node *temp;
+    temp=(struct Node *)malloc(sizeof(Node));
+    temp->data=key;
+    temp->previous=NULL;
+    temp->next=first;
+    first->previous=temp;
+    first=temp;
 
-void Insert(struct Node *p,int index,int key){
+}
+void addLast(struct Node *p,int key){
+    while(p->next){
+        p=p->next;
+    }
+    struct Node *temp;
+    temp=(struct Node *)malloc(sizeof(struct Node ));
+    temp->previous=p;
+    temp->next=NULL;
+    temp->data=key;
+    p->next=temp;
+
+}
+
+void InsertAfter(struct Node *p,int index,int key){
     struct Node *temp;
     if(index<0||index>length(p)){
         return;
     }
-    if(index==0){
-        temp=(struct Node *)malloc(sizeof(Node));
-        temp->data=key;
-        temp->previous=NULL;
-        temp->next=first;
-        first->previous=temp;
-        first=temp;
-    } else{
+    else{
         for(int i=0;i<index-1;i++){
             p=p->next;}
             temp=(struct Node *)malloc(sizeof(struct Node ));
@@ -68,8 +83,72 @@ void Insert(struct Node *p,int index,int key){
             temp->next=p->next;
             temp->previous=p;
 
-            p->next=temp;
+                p->next = temp;
 
+    }
+}
+
+void InsertBefore(struct Node *p,int index,int key){
+    struct Node *temp;
+    for(int i=0;i<index-2;i++){
+        p=p->next;}
+    temp=(struct Node *)malloc(sizeof(struct Node ));
+    temp->data=key;
+    temp->next=p->next;
+    temp->previous=p;
+
+    p->next = temp;
+
+
+}
+
+
+int  Search(struct Node *p,int key){
+
+ for(int i=0;i<length(p);i++){
+     if (p->data==key){
+         return i;
+     }
+     p=p->next;
+ }
+    return -1;
+}
+int Delete(struct Node *p,int index){
+    struct Node *temp;
+    int x;
+    if(index==1){
+
+        first=first->next;
+        if(first)first->previous=NULL;
+        x=p->data;
+        free(p);
+        return x;
+    }
+    else{
+        for(int i=0;i<index-1;i++){
+            p=p->next;
+        }
+        p->next->previous=p->previous;
+        p->previous->next=p->next;
+        x=p->data;
+        free(p);
+        return x;
+    }
+
+}
+
+void sort(struct Node *p){
+    int c=length(p),number[c];
+    for(int i=0;i<c;i++){
+        number[i]=p->data;
+        p=p->next;
+    }
+    p=first;
+
+    sort(number,number+c);
+    for(int i=0;i<c;i++){
+        p->data=number[i];
+        p=p->next;
     }
 }
 
@@ -81,10 +160,20 @@ int main() {
     Display(first);
 
     cout<<"The length is : "<<length(first)<<endl;
-    Insert(first,3,11);
+    InsertAfter(first,3,13);
+    Display(first);
+    InsertBefore(first,3,78);
+    Display(first);
+    addLast(first,39);
     Display(first);
 
+    cout<<"Index is : "<<Search(first,17);
+    cout<<endl;
+    Delete(first,1);
+    Display(first);
 
+    sort(first);
+    Display(first);
 
     return 0;
 }
