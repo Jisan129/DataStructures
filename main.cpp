04577494378189b8cf39da19d2092a9b1b441d35
Jisan129 <jishananam@iut-dhaka.edu>
2020-07-03 16:18:32 +0600

    efficient way of DSU

--- main.cpp
+++ main.cpp
@@ -1,28 +1,40 @@
 #include <bits/stdc++.h>
+#include <math.h>
 
 
 //implementing Disjoint set Union
 
 
 int parent[100];
+int size[100];
 
 void make_set(int i) {
     parent[i] = i;
+    size[i]=1;
 }
 
 int find_set(int i) {
     if (parent[i] == i)return i;
-    else {
-        find_set(parent[i]);
-    }
+
+    return parent[i] = find_set(parent[i]);     //stack implement ,every parent will be reassigned as the representative
+
 }
 
 void union_sets(int a, int b) {
 
-    int ra=find_set(a);
-    int rb=find_set(b);
-    if(ra!=rb){
-        parent[rb]=ra;
+    a = find_set(a);
+    b = find_set(b);
+    if (a != b) {
+
+        if(size[b]>size[a]){
+            int temp;
+            temp=a;
+            a=b;
+            b=temp;
+        }
+        parent[b] = a;
+        size[a]+=size[b];
+
     }
 
 }
@@ -33,16 +45,16 @@ int main() {
     make_set(4);
     make_set(2);
     make_set(3);
-    union_sets(1,2);
-    union_sets(3,4);
-    union_sets(1,3);
+    union_sets(1, 2);
+    union_sets(3, 4);
+    union_sets(1, 3);
     make_set(5);
     make_set(7);
     make_set(20);
 
-    union_sets(5,20);
+    union_sets(5, 20);
 
-    std::cout<<find_set(20);
+    std::cout << find_set(20);
 
     return 0;
 }
